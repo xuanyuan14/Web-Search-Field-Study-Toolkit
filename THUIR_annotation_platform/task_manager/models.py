5a29ecd07d8b79331d86f8e3450bb9bedc5b7df4
zwben <44130304+zwben@users.noreply.github.com>
2021-09-17 10:41:26 -0500

    Add files via upload

--- THUIR_annotation_platform/task_manager/models.py
+++ THUIR_annotation_platform/task_manager/models.py
@@ -11,7 +11,11 @@ except ImportError:
 
 
 class TaskAnnotation(models.Model):
-    user = models.ForeignKey(User)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        )
 
     # user intent
     specificity = models.IntegerField(default=-1)  # 0 ==> 4, broad ==> clear
@@ -31,8 +35,15 @@ class TaskAnnotation(models.Model):
 
 
 class Query(models.Model):
-    user = models.ForeignKey(User)
-    task_annotation = models.ForeignKey(TaskAnnotation)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        )
+    task_annotation = models.ForeignKey(
+        TaskAnnotation,
+        on_delete=models.CASCADE,
+        )
 
     partition_status = models.BooleanField(default=False)
     annotation_status = models.BooleanField(default=False)
@@ -64,8 +75,15 @@ class Query(models.Model):
 
 
 class PageLog(models.Model):
-    user = models.ForeignKey(User)
-    belong_query = models.ForeignKey(Query)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        )
+    belong_query = models.ForeignKey(
+        Query,
+        on_delete=models.CASCADE,
+        )
     page_type = models.CharField(max_length=50)
     page_title = models.CharField(max_length=50)
     origin = models.CharField(max_length=50)
@@ -85,7 +103,11 @@ class PageLog(models.Model):
 
 
 class QueryAnnotation(models.Model):  # !!
-    belong_query = models.ForeignKey(Query)
+    id = models.AutoField(primary_key=True)
+    belong_query = models.ForeignKey(
+        Query,
+        on_delete=models.CASCADE,
+        )
     # 1--initial, 2--spec, 3--gen, 4--meronym 5--holonym, 6--synonym, 7--parallel moving, 8--intent shift, 0--others
     relation = models.IntegerField()
     # 1--initial query ,2--SERP search snippets, 3--SERP other components,
@@ -98,7 +120,11 @@ class QueryAnnotation(models.Model):  # !!
 
 
 class SERPAnnotation(models.Model):
-    serp_log = models.ForeignKey(PageLog)
+    id = models.AutoField(primary_key=True)
+    serp_log = models.ForeignKey(
+        PageLog,
+        on_delete=models.CASCADE,
+        )
     usefulness_0 = models.CharField(max_length=1000)
     usefulness_1 = models.CharField(max_length=1000)
     usefulness_2 = models.CharField(max_length=1000)
